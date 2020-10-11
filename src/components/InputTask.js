2ee2f4acb90f24eb45a9b2ff19d33cc16b6fe987
Tony Griffin <tony@madetech.com>
2020-10-11 16:26:12 +0100

    Send body via fetch to backend to save data in postgres database

--- src/components/InputTask.js
+++ src/components/InputTask.js
@@ -2,17 +2,14 @@ import React, { Fragment, useState } from "react";
 
 const InputTask = () => {
   // set up component state
-  const [key, setKey] = useState(0);
-  const [description, setDescription] = useState(
-    localStorage.getItem("myValueInLocalStorage") || ""
-  );
+  const [description, setDescription] = useState("");
 
   const onChange = (event) => {
     setDescription(event.target.value);
   };
 
   // submit the form to send data out
-  const onSubmitForm = (event) => {
+  const onSubmitForm = async (event) => {
     event.preventDefault();
 
     const inputFieldValue = document.getElementById("input");
@@ -22,29 +19,21 @@ const InputTask = () => {
     }
 
     try {
-      let uniqueKey = key;
-      uniqueKey++;
-      setKey(uniqueKey);
-      // Check if localStorage already has this key, get an array of the keys and check if its in it,includes()?
-      let localStorageKeys = Object.keys(localStorage).sort();
-
-      // If true, set the new key to the length of the localStorage array (one greater than the last element).
-      if (localStorageKeys.includes(uniqueKey.toString())) {
-        let key = localStorage.length + 1;
-        localStorage.setItem(key, description);
-      } else {
-        // If not just add the key
-        localStorage.setItem(uniqueKey, description);
-      }
-      window.location.reload();
-    } catch (err) {
-      console.error(err.message);
+      const body = { description };
+      const response = await fetch("http://localhost:5000/tasks", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      console.log(response);
+    } catch (error) {
+      console.error(error.message);
     }
   };
 
   return (
     <Fragment>
-      <h2 className="text-center mt-5">Add Task</h2>
+      <h2 className="text-center mt-5">Input Task</h2>
       <form className="d-flex mt-5" onSubmit={onSubmitForm}>
         <label htmlFor="input" />
         <input
