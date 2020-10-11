f86215e7c053455f92957a3d51c43939c8a31944
Tony Griffin <tony@madetech.com>
2020-10-11 17:06:15 +0100

    Add window refresh after POST requedst

--- src/components/InputTask.js
+++ src/components/InputTask.js
@@ -25,7 +25,7 @@ const InputTask = () => {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(body),
       });
-      console.log(response);
+      window.location = "/";
     } catch (error) {
       console.error(error.message);
     }
