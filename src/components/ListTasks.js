097a6129c58d4af3499358088bca11fab88eba11
Tony Griffin <tony@madetech.com>
2020-10-11 17:05:39 +0100

    Add fetch tasks from db and set to state within ListTasks component, use useEffect to rerender the compoent on update of state. Map through returned tasks and rendered them. Add window redirect to InputTask after POST request

--- src/components/ListTasks.js
+++ src/components/ListTasks.js
@@ -1,6 +1,24 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 
 const ListTasks = (props) => {
+  const [tasks, setTasks] = useState([]);
+
+  const getTasks = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/tasks");
+      const jsonData = await response.json();
+      setTasks(jsonData);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getTasks();
+  }, []);
+
+  console.log(tasks);
+
   return (
     <Fragment>
       <table className="table mt-5 text-center">
@@ -13,10 +31,10 @@ const ListTasks = (props) => {
           </tr>
         </thead>
         <tbody>
-          {props.taskItems.map((task) => {
+          {tasks.map((task) => {
             return (
-              <tr key={task.id}>
-                <td>{task.id}</td>
+              <tr key={task.task_id}>
+                <td>{task.task_id}</td>
                 <td>{task.description}</td>
                 <td>Edit</td>
                 <td>Delete</td>
